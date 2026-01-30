c1ad1d5149a96e28fdb3566c23792777b2e39cbf
Manan1121 <mananhshah04@gmail.com>
2026-01-30 16:39:15 -0500

    Added game loop and directional input, however it is currently updating too fast

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -13,15 +13,24 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    KeyHandler keyH = new KeyHandler();
+
     Thread gameThread;
 
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
+
+
 
 
     public void startGameThread() {
@@ -33,15 +42,41 @@ public class GamePanel extends JPanel implements Runnable{
     public void run() {
         while (gameThread != null) {
             //System.out.println("The Game is Running");
+            //UPDATE
+            update();
+            //DRAW
+            repaint();
         }
     }
     public void update(){
+        if (keyH.upPressed == true){
+            playerY -= playerSpeed;
+        } else if (keyH.downPressed == true) {
+            playerY += playerSpeed;
+        }
+         else if (keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        }
+        else if (keyH.rightPressed == true) {
+            playerX += playerSpeed;
 
     }
+        }
+
 
     public void paintComponent(Graphics g){
+        //Standard built in Java method standard methods to draw things in JPanel in Java
+        //Graphics Class used to draw things on screen
+        //Super means the parent class of paintcomponent which is JPanel
         super.paintComponent(g);
 
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
 
     }
 }
